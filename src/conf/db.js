d42bc9abc96432c9e0d03cf44cd1e7e56cce2375
Allen Bai <957820957@qq.com>
2019-03-20 14:38:45 +0800

    add redis server configuration adn encapsulate set get function for operating redis

--- src/conf/db.js
+++ src/conf/db.js
@@ -1,14 +1,21 @@
 const env = process.env.NODE_ENV;
 
 let MYSQL_CONF;
+let REDIS_CONF;
 
 if (env === "dev") {
+  // mysql
   MYSQL_CONF = {
     host: "localhost",
     port: 3306,
     user: "root",
     password: "123",
     database: "myblog"
+  };
+  // redis
+  REDIS_CONF = {
+    port: 6379,
+    host: "192.168.72.129"
   };
 }
 
@@ -20,8 +27,14 @@ if (env === "development") {
     password: "123",
     database: "myblog"
   };
+  // redis
+  REDIS_CONF = {
+    port: 6379,
+    host: "192.168.72.129"
+  };
 }
 
 module.exports = {
-  MYSQL_CONF
+  MYSQL_CONF,
+  REDIS_CONF
 };
