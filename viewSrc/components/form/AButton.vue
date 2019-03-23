514bf52fe5ac5960dc40b9dab4248657af3f7ebc
Allen Bai <957820957@qq.com>
2019-03-23 16:42:26 +0800

    add AButton AInput component

---
+++ viewSrc/components/form/AButton.vue
@@ -0,0 +1,112 @@
+<template>
+  <button
+    type="button"
+    class="a-button--transparent a-button"
+    :class="[`a-button-${type}`,`a-button-${size}`]"
+    v-bind="$attrs"
+  >
+    <div class="a-button-wrapper flex-around">
+      <div class="a-ripple"></div>
+      <span>
+        <slot></slot>
+      </span>
+    </div>
+  </button>
+</template>
+<script>
+export default {
+  name: "a-button",
+  props: {
+    type: {
+      type: String,
+      default: "primary"
+    },
+    size: {
+      type: String,
+      default: "md" // sm md lg
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "./var.scss";
+
+.a-button {
+  color: #fff;
+  border-radius: 2px;
+  overflow: hidden;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.7);
+  cursor: pointer;
+  &:active {
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.8);
+    .a-ripple {
+      opacity: 0.15;
+    }
+  }
+}
+
+.a-button-wrapper {
+  position: relative;
+  padding: 0 10px;
+}
+.a-ripple {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  opacity: 0;
+  background-color: #fff;
+}
+
+.a-button-sm {
+  font-size: 12px;
+  line-height: 28px;
+}
+
+.a-button-md {
+  font-size: 14px;
+  line-height: 34px;
+}
+
+.a-button-lg {
+  font-size: 16px;
+  line-height: 40px;
+}
+
+.a-button-primary {
+  background-color: $primary;
+}
+.a-button-primary-dark {
+  background-color: $primary-dark;
+}
+.a-button-primary-light {
+  background-color: $primary-light;
+}
+.a-button-secondary {
+  background-color: $secondary;
+}
+.a-button-secondary-dark {
+  background-color: $secondary-dark;
+}
+.a-button-secondary-light {
+  background-color: $secondary-light;
+}
+.a-button-success {
+  background-color: $success;
+}
+.a-button-warning {
+  background-color: $warning;
+}
+.a-button-danger {
+  background-color: $danger;
+}
+.a-button-info {
+  background-color: $info;
+}
+.a-button-plain {
+  background-color: #fff;
+  border: 1px solid $border;
+  color: $regular-text;
+}
+</style>
--- viewSrc/components/form/AInput.vue
+++ viewSrc/components/form/AInput.vue
@@ -1,15 +1,125 @@
 <template>
   <div class="a-input-field">
-    <div class="a-input-label">用户名：</div>
-    <div class="flex-between">
-      <input type="text">
-      <div class="a-input-tail">尾部</div>
-    </div>
+    <div class="a-input-title" v-if="title">{{title}}</div>
+    <label class="a-input-label flex-between">
+      <button @click="clickLeft" v-if="leftIcon" class="a-input-icon-btn a-button--transparent">
+        <AIcon class="a-input-icon" :name="leftIcon"/>
+      </button>
+      <input v-bind="$attrs" class="a-input" :type="type" :value="value" @input="inputHandler">
+      <button
+        @click="clearInput"
+        v-if="showClearBtn"
+        class="a-input-icon-btn a-button--transparent"
+      >
+        <AIcon class="a-input-icon" name="close-circle-fill"/>
+      </button>
+      <button @click="clickRight" v-if="rightIcon" class="a-input-icon-btn a-button--transparent">
+        <AIcon class="a-input-icon" :name="rightIcon"/>
+      </button>
+      <button class="a-button--transparent">
+        <slot name="right"></slot>
+      </button>
+    </label>
     <div class="a-input-error"></div>
   </div>
 </template>
 <script>
+import AIcon from "../AIcon";
 export default {
-  name: "a-input"
+  name: "a-input",
+  components: {
+    AIcon
+  },
+  props: {
+    rightIcon: String,
+    leftIcon: String,
+    value: String,
+    title: String,
+    type: {
+      type: String,
+      default: "text"
+    },
+    clearable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    showClearBtn() {
+      return this.clearable && this.value.length > 0;
+    }
+  },
+  methods: {
+    clickLeft(evt) {
+      return this.$listeners.clickLeft && this.$listeners.clickLeft(evt);
+    },
+    clickRight(evt) {
+      return this.$listeners.clickRight && this.$listeners.clickRight(evt);
+    },
+    inputHandler(evt) {
+      this.$emit("input", evt.target.value);
+    },
+    clearInput() {
+      this.$emit("input", "");
+    }
+  }
 };
 </script>
+<style lang="scss">
+@import "./var.scss";
+
+.a-input-title {
+  font-size: 14px;
+  color: $primary;
+}
+.a-input-error {
+  $h: 24px;
+  color: $danger;
+  height: $h;
+  line-height: $h;
+  box-sizing: border-box;
+}
+
+// 普通状态
+.a-input-label {
+  border-bottom: 2px solid $border;
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    left: 50%;
+    bottom: -2px;
+    height: 2px;
+    width: 0;
+    background-color: $primary;
+    transition: all 0.4s;
+  }
+  .a-input {
+    padding: 8px 0;
+    width: 100%;
+    flex: 1;
+    color: $title-text;
+  }
+  .a-input-icon-btn {
+    padding: 5px 2px;
+  }
+  .a-input-icon {
+    font-size: 18px;
+    color: $caption-text;
+  }
+  button {
+    padding: 8px 2px;
+  }
+}
+
+// 聚焦状态
+.a-input-label:focus-within {
+  &::after {
+    left: 0;
+    width: 100%;
+  }
+  .a-input-icon {
+    color: $primary;
+  }
+}
+</style>
